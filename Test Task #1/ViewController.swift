420ec501487d67fe3c2fa47783e396fe57829fde
Pavel <Pavel.818@yandex.ru>
2022-10-21 10:35:09 +0500

    Initial Commit

---
+++ Test Task #1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test Task #1
+//
+//  Created by TestTest on 21.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
